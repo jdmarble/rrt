c31b9a78472d9ff85cdc162b64a7a22a08d033fc
J.D. Marble <jmarble@cse.unr.edu>
2010-09-16 16:06:59 -0700

    Cleaned up Maybes in RRT.

--- src/Data/RRT.hs
+++ src/Data/RRT.hs
@@ -1,6 +1,12 @@
 {-# LANGUAGE TypeFamilies #-}
 
-module Data.RRT (Config (..), search, samples, controlEdgeProp) where
+module Data.RRT
+	( Config (..)
+	, search
+	, maybeSearch
+	, samples
+	, controlEdgeProp
+	) where
 
 import Data.List (minimumBy)
 import Data.List.Split (splitEvery)
@@ -13,79 +19,82 @@ import Data.SpatialIndex
 
 data (Real real, SpatialIndex index, Element index ~ state)
     => Config state control index real edge = Config
-    { -- | Propagation function
-      propagation :: state -> control -> (edge, state)
+    { -- | Propagation function returns Nothing if the control is not feasible.
+      propagation :: state -> control -> Maybe (edge, state)
       -- | Spatial index containing the initial state
     , spacialIndex :: index
       -- | Metric for measuring which control results in a state
       -- closest to a sample state
     , controlMetric :: state -> state -> real
-      -- | Collision detection function. Paths with collisions will be rejected.
-    , collided :: state -> Bool
     }
 
-controlEdgeProp :: (s -> c -> s) -> (s -> c -> (c, s))
-controlEdgeProp = (ap (,) .)
+-- | Convert a simple propagation function to one that returns the control
+-- as an edge.
+controlEdgeProp :: (s -> c -> s) -> (s -> c -> Maybe (c, s))
+controlEdgeProp f s c = Just (c, f s c)
+
+
 
 -- | Create a Rapidly exploring Random Tree (RRT) given a propagation
 -- function, an initial state, and some randomly sampled states and
 -- controls. The tree is returned as a list of (parent, edge, child)
 -- triples which can be used as-is or placed into a proper tree
 -- structure.
-search :: (Real r, SpatialIndex index, Element index ~ state, Eq state)
+search :: (Real r, SpatialIndex index, Element index ~ state)
        => Config state control index r edge -- ^ Configuration parameters
        -> [(state, [control])]        -- ^ Random states and controls
        -> [(edge, state)]             -- ^ Resulting tree as a list of nodes
-search config stream = catMaybes $ fst 
-                     $ runState (mapM (explore config) stream) 
-                     $ spacialIndex config
+search = (catMaybes .) . maybeSearch
+
+
+
+maybeSearch :: (Real r, SpatialIndex index, Element index ~ state)
+            => Config state control index r edge -- ^ Configuration parameters
+            -> [(state, [control])]       -- ^ Random states and controls
+            -> [Maybe (edge, state)]      -- ^ Resulting tree as a list of nodes
+maybeSearch config stream = fst
+                          $ runState (mapM (explore config) stream)
+                          $ spacialIndex config
 
 
 -- | Make a single step in the RRT algorithm updating intermediate state.
-explore :: (Real r, SpatialIndex index, Element index ~ state, Eq state)
+explore :: (Real r, SpatialIndex index, Element index ~ state)
         => Config state control index r edge -- ^ Configuration parameters
         -> (state, [control])                -- ^ Random sample
         -> State index (Maybe (edge, state)) -- ^ New tree node
 explore config sample =
     do index <- get
        let maybeEdge = expand config index sample
-       if isJust maybeEdge 
-         then put $ insert (snd $ fromJust maybeEdge) index
-         else return ()
+       maybe (return ())
+             (\edge -> put $ insert (snd edge) index)
+             maybeEdge
        return maybeEdge
 
 
 -- | Given a spatial index of all the states in a tree, return a new edge.
-expand :: (Real r, SpatialIndex index, Element index ~ state, Eq state)
+expand :: (Real r, SpatialIndex index, Element index ~ state)
        => Config state control index r edge -- ^ Configuration parameters
        -> index                       -- ^ For performing nearest neighbor query
        -> (state, [control])          -- ^ State to try to expand to using one
                                       -- of the possible controls
        -> Maybe (edge, state)         -- ^ New tree edge with new child node
-expand config index (sample, controls) = if selfEdge then Nothing else node
+expand config index (sample, controls) = node
     where
       parent = index `nearest` sample
       node = genChoices (propagation config parent)
                         (controlMetric config sample)
-                        (not . collided config)
                         controls
-      selfEdge = if isJust node
-                   then (snd $ fromJust node) == parent -- Check for self edge
-                   else False
-                         -- Self-edge means prop couldn't advance the state
-                         -- This is such a mess. Do it in the Maybe monad.
 
 -- | 
 genChoices :: Real r
-           => (control -> (edge, state)) -- ^ Propagation function
-           -> (state -> r)               -- ^ Distance to desired state
-           -> (state -> Bool)            -- ^ True if not colliding
-           -> [control]                  -- ^ Possible controls
-           -> Maybe (edge, state)        -- ^ Edge and the resulting state
-genChoices prop distance notColliding controls = choose choices
+           => (control -> Maybe (edge, state)) -- ^ Propagation function
+           -> (state -> r)                     -- ^ Distance to desired state
+           -> [control]                        -- ^ Possible controls
+           -> Maybe (edge, state)              -- ^ Edge and the resulting state
+genChoices prop distance controls = choose choices
     where
       -- The (edge, state)s achieved after applying all controls
-      (edges, states) = unzip $ filter (notColliding . snd) $ map prop controls
+      (edges, states) = unzip $ catMaybes $ map prop controls
       -- The distance of each of the new states to the sample
       distances = map distance states
       -- Bundle everything together so the closest (control,state) can
@@ -94,7 +103,7 @@ genChoices prop distance notColliding controls = choose choices
 
 -- | Return the closest state to the sample along with the control
 -- that causes it to propagate there.
-choose :: Real r => [(r, (edge, state))] -> Maybe (edge, state)
+choose :: Ord a => [(a, b)] -> Maybe b
 choose [] = Nothing
 choose choices = Just $ snd $ minimumBy (comparing fst) choices
 
--- test/RRTTest.hs
+++ test/RRTTest.hs
@@ -16,6 +16,6 @@ tests = $(testGroupGenerator)
 
 
 prop_num_streams_eq_num_nodes stream = length tree == length stream
-    where config = Config (controlEdgeProp (+)) [0] distance $ const False
-          tree = search config stream
+    where config = Config (controlEdgeProp (+)) [0] distance
+          tree = maybeSearch config stream
           types = (stream :: [(Integer, [Integer])])
