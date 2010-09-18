e6321a4a9ca8f2cf20d05693273a7393f21fefca
J.D. Marble <jmarble@cse.unr.edu>
2010-09-17 17:00:59 -0700

    Test failure of rrt to propagate.

--- test/RRTTest.hs
+++ test/RRTTest.hs
@@ -19,3 +19,9 @@ prop_num_streams_eq_num_nodes stream = length tree == length stream
     where config = Config (controlEdgeProp (+)) [0] distance
           tree = maybeSearch config stream
           types = (stream :: [(Integer, [Integer])])
+
+
+prop_failed_means_empty_tree stream = null tree
+    where config = Config (const $ const Nothing) [0] distance
+          tree = search config stream
+          types = (stream :: [(Integer, [Integer])])
